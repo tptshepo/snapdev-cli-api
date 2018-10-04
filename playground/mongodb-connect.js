ee1aa003c3d2dac45b9805e82a9503fa5fad5a28
Tshepo Mgaga <tshepo.mgaga@gmail.com>
2018-10-04 17:54:20 +0200

    First commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,34 @@
+const MongoClient = require('mongodb').MongoClient;
+// Connection url
+const url = 'mongodb://localhost:27017';
+const dbName = 'SnapDev';
+
+// Connect using MongoClient
+MongoClient.connect(
+  url,
+  (err, client) => {
+    if (err) {
+      return console.log('Unable to connect to mongodb.');
+    }
+    console.log('Connected to mongodb');
+
+    const db = client.db(dbName);
+
+    db.collection('Packages').insertOne(
+      {
+        name: 'CSharp',
+        tags: ['.net', 'core']
+      },
+      (err, result) => {
+        if (err) {
+          return console.log('Error inserting a package', err);
+        }
+        console.log(
+          JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2)
+        );
+      }
+    );
+
+    client.close();
+  }
+);
---
+++ playground/mongodb-find.js
@@ -0,0 +1,50 @@
+const { MongoClient, ObjectID } = require('mongodb');
+// Connection url
+const url = 'mongodb://localhost:27017';
+const dbName = 'SnapDev';
+
+// Connect using MongoClient
+MongoClient.connect(
+  url,
+  (err, client) => {
+    if (err) {
+      return console.log('Unable to connect to mongodb.');
+    }
+    console.log('Connected to mongodb');
+
+    const db = client.db(dbName);
+
+    // db.collection('Packages')
+    //   .find({
+    //     // name: 'Android'
+    //     _id: new ObjectID('5bb634561933cd2944b29fee')
+    //   })
+    //   .toArray()
+    //   .then(
+    //     docs => {
+    //       console.log(JSON.stringify(docs, undefined, 2));
+    //     },
+    //     err => {
+    //       if (err) {
+    //         console.log('Error finding document');
+    //       }
+    //     }
+    //   );
+
+    db.collection('Packages')
+      .find()
+      .count()
+      .then(
+        count => {
+          console.log(`Total count: ${count}`);
+        },
+        err => {
+          if (err) {
+            console.log('Error finding document');
+          }
+        }
+      );
+
+    // client.close();
+  }
+);
